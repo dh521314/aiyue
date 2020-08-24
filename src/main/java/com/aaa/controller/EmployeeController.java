b3c2f0096ea874d47f62865a5dafb7244a82251d
cs <2931119681@qq.com>
2020-08-24 15:07:49 +0800

    Revert "cs"
    This reverts commit 2a26d17c

--- src/main/java/com/aaa/controller/EmployeeController.java
+++ src/main/java/com/aaa/controller/EmployeeController.java
@@ -1,4 +1,18 @@
 package com.aaa.controller;
 
+import com.aaa.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("Emp")
 public class EmployeeController {
+    @Resource
+    EmployeeService employeeService;
+
+
+
+
 }
--- src/main/java/com/aaa/controller/TypeController.java
+++ src/main/java/com/aaa/controller/TypeController.java
@@ -1,4 +1,18 @@
 package com.aaa.controller;
 
+import com.aaa.service.TypeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("Type")
 public class TypeController {
+    @Resource
+    TypeService typeService;
+
+
+
+
 }
