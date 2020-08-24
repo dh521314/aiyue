e291c964da032dee0f2cb0125ee3545a2e8c6fa7
zkh521 <2716000283@qq.com>
2020-08-24 18:24:29 +0800

    12

--- src/main/java/com/aaa/controller/TypeController.java
+++ src/main/java/com/aaa/controller/TypeController.java
@@ -1,17 +1,40 @@
 package com.aaa.controller;
 
+import com.aaa.entity.Type;
 import com.aaa.service.TypeService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("Type")
 public class TypeController {
     @Resource
     TypeService typeService;
 
+    @RequestMapping("add")
+    public Integer add(Type type) {
+        return typeService.add(type);
+    }
+
+    @RequestMapping("del")
+    public Integer del(Integer tid) {
+        return typeService.del(tid);
+    }
+
+    @RequestMapping("update")
+    public Integer update(Type type) {
+        return typeService.update(type);
+    }
+
+    @RequestMapping("findAll")
+    public List<Type> findAll() {
+        return typeService.findAll();
+    }
 
 
 
