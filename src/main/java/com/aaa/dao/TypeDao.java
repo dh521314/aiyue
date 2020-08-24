2a26d17c1f5d8d1d392f7b40f2eb87b00279687d
cs <2931119681@qq.com>
2020-08-24 14:29:43 +0800

    cs

---
+++ src/main/java/com/aaa/dao/TypeDao.java
@@ -0,0 +1,8 @@
+package com.aaa.dao;
+
+import com.aaa.entity.Type;
+import tk.mybatis.mapper.common.Mapper;
+
+@org.apache.ibatis.annotations.Mapper
+public interface TypeDao extends Mapper<Type> {
+}
---
+++ src/main/java/com/aaa/service/TypeService.java
@@ -0,0 +1,17 @@
+package com.aaa.service;
+
+import com.aaa.entity.Type;
+
+import java.util.List;
+
+public interface TypeService {
+
+    public Integer add(Type type);
+
+    public Integer del(Integer tid);
+
+    public Integer update(Type type);
+
+    public List<Type> findAll();
+
+}
