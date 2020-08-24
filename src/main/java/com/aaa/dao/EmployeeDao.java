18d7ad737aa49bf9f7845a934f31de6e6ba47505
cs <2931119681@qq.com>
2020-08-24 15:12:39 +0800

    Revert "cs"
    This reverts commit 2a26d17c

--- src/main/java/com/aaa/dao/EmployeeDao.java
+++ src/main/java/com/aaa/dao/EmployeeDao.java
@@ -1,12 +1,14 @@
 package com.aaa.dao;
 
 import com.aaa.entity.Employee;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
 @org.apache.ibatis.annotations.Mapper
 public interface EmployeeDao extends Mapper<Employee> {
+    @Select("select * from employee where ename = #{param1} and epwd = #{param2}")
     public List<Employee> login(String ename,String epwd);
 
 }
