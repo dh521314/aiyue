f63fdeda084f0d930f4ac6c265cfaba7e4c409c4
cs <2931119681@qq.com>
2020-08-25 02:51:56 +0800

    menu

--- src/main/java/com/aaa/dao/MenuDao.java
+++ src/main/java/com/aaa/dao/MenuDao.java
@@ -1,10 +1,12 @@
 package com.aaa.dao;
 
 import com.aaa.entity.Menu;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
+@Mapper
 public interface MenuDao {
 
     @Select("select m.* from menu m inner join menu_post mp on m.mid = mp.menuid inner join post p on p.pid = mp.postid where pid = #{param1}")
