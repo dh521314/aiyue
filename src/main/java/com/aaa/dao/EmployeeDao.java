b9a8183cde47a9887c9b2a5ff1d6d8b393c4ff85
trace00 <2215177759@qq.com>
2020-09-01 09:03:01 +0800

    04

--- src/main/java/com/aaa/dao/EmployeeDao.java
+++ src/main/java/com/aaa/dao/EmployeeDao.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.annotations.*;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
-import java.util.Map;
 
 @org.apache.ibatis.annotations.Mapper
 public interface EmployeeDao extends Mapper<Employee> {
@@ -30,14 +29,19 @@ public interface EmployeeDao extends Mapper<Employee> {
     @Update("update employee set state=#{state} where eid=#{eid}")
     public Integer updateEmp(Integer eid,Integer state);
 
-    //根据姓名查询
-    @Select("select e.eid,e.ename,e.epwd,e.realname,e.idcard,e.ephoto,e.ephone,e.email,p.pname,e.state from employee e left join post p on e.postid=p.pid where e.realname like '%${realname}%'")
-    public List<Map<String,Object>> findByName(String realname);
-
     //根据用户名查询
     @Select("select * from employee where ename = #{param1}")
     public Employee queryEmpName(String ename);
 
+    //根据姓名查询
+    /*@Select("select e.eid,e.ename,e.epwd,e.realname,e.idcard,e.ephoto,e.ephone,e.email,p.pname,e.state from employee e left join post p on e.postid=p.pid where e.realname like '%${realname}%'")*/
+    @Select("select * from employee where realname like '%${realname}%'")
+    @Results({
+            @Result(property = "post",column = "postid",one = @One(select = "com.aaa.dao.PostDao.findPid"))
+    })
+    public List<Employee> findByName(String realname);
+
+    //查询员工信息
     @Select("select * from employee")
     @Results({
             @Result(property = "post",column = "postid",one = @One(select = "com.aaa.dao.PostDao.findPid"))
